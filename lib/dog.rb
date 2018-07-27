49088714d5756db092e5bf5a7420c1919df2654b
Karissa Bui <karissab002@gmail.com>
2018-07-27 16:48:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # dog.rb
+
+class Dog (fido, snoopy, lassie)
+  def initialize
+    @fido = fido
+    @snoopy = snoopy
+    @lassie = lassie
+  end
+  def fido 
+   @fido
+  end
+end
+
+dog_one = Dog.new("fido", "snoopy","lassie")
+
+puts dog_one.name 
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # person.rb
+
+class Person()
+  def initialize
+    @adele_goldberg = adele goldberg 
+    @alan_kay = alan kay
+  end
+  def adele goldberg
+    @adele_goldberg
+  end
+end
+
+person_one = Person.new("adele_goldberg", "alan kay")
+
+puts person_one.name
+
+
+    
